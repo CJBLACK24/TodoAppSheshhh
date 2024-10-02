0341b8709e6f9e1956a8960ef05d7d32abffdf4b
DUQUE CHRISTIAN JOHN C <117024837+CJBLACK24@users.noreply.github.com>
2024-10-02 08:03:56 +0800

    Update project with latest changes

--- server/index.js
+++ server/index.js
@@ -16,10 +16,10 @@ app.use("/api", router);
 
 const port = process.env.PORT || 5000;
 
-const startServer = async () => {
+async function startServer() {
     await connectToMongoDB();
     app.listen(port, () => {
         console.log(`Server is listening on http://localhost:${port}`);
     });
-};
+}
 startServer();
